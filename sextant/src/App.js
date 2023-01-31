03bcb05f953c39f939e0ee214241a0cd8abcb710
thanhquan1704@gmail.com <quannguyen@Quans-MacBook-Pro.local>
2023-01-30 21:37:47 -0800

    Adding banner

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const Banner = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.bannerContainer}>
+      <h1 style={styles.bannerText}>IP's latency information</h1>
     </div>
   );
+};
+
+const styles = {
+  bannerContainer: {
+    backgroundColor: "lightgray",
+    padding: "20px",
+    textAlign: "center",
+  },
+  bannerText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+};
+
+function App() {
+  return <Banner></Banner>;
 }
 
 export default App;
